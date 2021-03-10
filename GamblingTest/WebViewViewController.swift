26a12125803cf7392d1d731dcda40cc597438fc8
Oksana <klassi4eske@gmail.com>
2021-03-10 16:54:26 +0200

    Empty page vs Webview

--- GamblingTest/WebViewViewController.swift
+++ GamblingTest/WebViewViewController.swift
@@ -10,8 +10,45 @@ import UIKit
 
 class WebViewViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK:  Properties
+    
+    private let webView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+    
+    // MARK:  Methods
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.addSubview(webView)
+        guard let url = URL(string: "https://www.google.com") else {
+            return }
+        webView.load(URLRequest(url: url))
+        webView.navigationDelegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+}
+
+// MARK:  WKNavigationDelegate
+
+extension WebViewViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if let host = navigationResponse.response.url?.host {
+            if host.contains("google.com") {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        decisionHandler(.cancel)
+        webView.removeFromSuperview()
+    }
 }
